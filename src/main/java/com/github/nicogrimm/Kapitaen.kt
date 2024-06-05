34eedb0f254ec945bd71df67f89573713a855049
Nico Grimm <nico.grimm@axelspringer.com>
2024-06-05 10:50:17 +0200

    feat: begrenze die Erfahrung und das Charisma eines Kapitäns zwischen 1 und 10

--- src/main/java/com/github/nicogrimm/Kapitaen.kt
+++ src/main/java/com/github/nicogrimm/Kapitaen.kt
@@ -3,9 +3,30 @@ package com.github.nicogrimm
 import kotlin.math.floor
 import kotlin.random.Random
 
-class Kapitaen(var name: String, var charisma: Int, var erfahrung: Int) {
+class Kapitaen(var name: String, charisma: Int, erfahrung: Int) {
     var raumschiff: Raumschiff? = null
 
-    fun diplomatischeVerhandlung() =
+    var charisma: Int = 0
+        set(value) {
+            if (value !in 1..10) {
+                throw IllegalArgumentException("Charisma muss im Bereich von 1 bis 10 sein")
+            }
+            field = value
+        }
+
+    var erfahrung: Int = 0
+        set(value) {
+            if (value !in 1..10) {
+                throw IllegalArgumentException("Erfahrung muss im Bereich von 1 bis 10 sein")
+            }
+            field = value
+        }
+
+    init {
+        this.charisma = charisma
+        this.erfahrung = erfahrung
+    }
+
+    fun diplomatischeVerhandlungsWert() =
         floor(erfahrung * 2 + charisma * (Random.nextInt(5, 15).toDouble() / 10)).toInt()
 }
